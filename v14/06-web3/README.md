1e2197a639d762e2591475a5ef7659d0b84ff3a0
xdrkush <drkushikush@gmail.com>
2022-12-27 20:44:05 +0100

    clean

--- v14/06-web3/README.md
+++ v14/06-web3/README.md
@@ -2,6 +2,27 @@
 
 You can interact with the contract KushToken.
 
-source:
+## INFO:
+
+this contract : 0xF2FC023D33577Dfe3c9C3422Bd5f9344824b0489
+Is KushToken on eth goerli, just for test.
+https://goerli.etherscan.io/token/0xF2FC023D33577Dfe3c9C3422Bd5f9344824b0489
+
+## env
+
+CLIENT_ID=info->../README.md
+GUILD_ID=info->../README.md
+TOKEN_DISCORD=info->../README.md
+
+ETH_ACCOUNT_PUBLIC=yourpublicaddress
+ETH_ACCOUNT_PRIVATE=onlyIfYouWantInteractWithOwnerOnYourContract
+ETH_CONTRACT=0xF2FC023D33577Dfe3c9C3422Bd5f9344824b0489
+
+KEY_INFURA=infura_key_eth_goerli
+
+
+## source:
+  - https://infura.io/
   - https://hardhat.org/tutorial
   - https://web3js.readthedocs.io/en/v1.7.5/getting-started.html
+
--- v14/06-web3/config/web3.js
+++ v14/06-web3/config/web3.js
@@ -1,11 +1,62 @@
-require('dotenv').config()
-const Web3 = require("web3")
-const abi = require('./abis/KushToken.json')
+require('dotenv').config();
+const Web3 = require("web3");
+const { Chain, Common, Hardfork } = require('@ethereumjs/common');
+const { Transaction } = require('@ethereumjs/tx');
+const rxjs = require('rxjs');
 
-const { ETH_ACCOUNT_PUBLIC, ETH_ACCOUNT_PRIVATE, ETH_CONTRACT, KEY_INFURA } = process.env
+const Tx = Transaction;
 
-const web3 = new Web3(new Web3.providers.HttpProvider(`https://goerli.infura.io/v3/${ KEY_INFURA }`))
-const KushToken = new web3.eth.Contract(abi, ETH_CONTRACT, { from: ETH_ACCOUNT_PUBLIC, gas: 100000 })
+const abi = require('./abis/KushToken.json');
+const abiFaucet = require('./abis/KushTokenFaucet.json');
+
+const { ETH_ACCOUNT_PUBLIC, ETH_ACCOUNT_PRIVATE, ETH_CONTRACT, ETH_CONTRACT_FAUCET, KEY_INFURA } = process.env
+
+// Web3
+const web3 = new Web3(new Web3.providers.HttpProvider(`https://goerli.infura.io/v3/${KEY_INFURA}`));
+const web3Account = web3.eth.accounts.privateKeyToAccount(ETH_ACCOUNT_PRIVATE);
+web3.eth.accounts.wallet.add(web3Account);
+web3.eth.defaultAccount = web3Account.address;
+
+console.log('web3Account', web3Account);
+
+// Contract
+const KushToken = new web3.eth.Contract(abi, ETH_CONTRACT);
+const KushTokenFaucet = new web3.eth.Contract(abiFaucet, ETH_CONTRACT_FAUCET);
+
+// console.log('ICICICI', KushToken.methods)
+// console.log('ICICICI', KushTokenFaucet.methods)
+
+const addressTest = "0x1Ff062a7647FcaE2d43005Bf13CC00Ff96E581b6";
+
+// const common = new Common({ chain: 'goerli', hardfork: 'london' })
+
+const privateKeyBUF = Buffer.from(ETH_ACCOUNT_PRIVATE.replace(/^0x/, ''), 'hex');
+rxjs.from(web3.eth.getTransactionCount(web3Account.address)).pipe(
+    rxjs.switchMap((count) => {
+        const rawTransaction = {
+            from: toAccount,
+            gasPrice: web3.utils.toHex(20 * 1e9),
+            gasLimit: web3.utils.toHex(210000),
+            to: addressTest,
+            value: 0x0,
+            data: contract.methods.dripSby(toAccount).encodeABI(),
+            nonce: web3.utils.toHex(count),
+        };
+
+        const transaction = new Tx(rawTransaction, { common: customCommon });
+        transaction.sign(privateKeyBUF);
+
+        return rxjs.from(
+            web3.eth.sendSignedTransaction(
+                '0x' + transaction.serialize().toString('hex')
+            )
+        );
+    }),
+    rxjs.map((transaction) => {
+        console.log(transaction)
+        return transaction;
+    })
+);
 
 module.exports = {
     web3,
@@ -19,5 +70,5 @@ module.exports = {
             addressContract: ETH_CONTRACT,
             addressDeployer: ETH_ACCOUNT_PUBLIC
         }
-    },
+    }
 }
